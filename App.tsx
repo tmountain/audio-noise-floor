6b7d1ee3bbcbda1fa02872527fe19568eab073bf
Travis Whitton <tinymountain@gmail.com>
2024-02-14 14:28:37 +0000

    two languages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React, { useRef, useState } from "react";
 import { StyleSheet, View, Button, Text } from "react-native";
-import { Video, AVPlaybackStatus } from "expo-av";
-import { Audio, ResizeMode } from "expo-av";
+import { Video, AVPlaybackStatus, ResizeMode } from "expo-av";
+import { Audio } from "expo-av";
+
+type Language = "EN" | "ES";
 
 export default function App() {
   const video = useRef<Video>(null);
   const [status, setStatus] = useState<AVPlaybackStatus | {}>({});
   const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [language, setLanguage] = useState<Language>("EN");
 
   const loadAudio = async (audioURL: string) => {
     const { sound } = await Audio.Sound.createAsync(
@@ -17,17 +20,18 @@ export default function App() {
   };
 
   const playVideoAndAudio = async (volume: number) => {
-    // Assuming you have a URL for the video and audio
-    const videoURL: string =
-      "https://nt-audio-test.s3.us-east-2.amazonaws.com/word_memory.mp4";
-    const audioURL: string =
+    const videoURLs: Record<Language, string> = {
+      EN: "https://nt-audio-test.s3.us-east-2.amazonaws.com/word_memory_english.mp4",
+      ES: "https://nt-audio-test.s3.us-east-2.amazonaws.com/word_memory_spanish.mp4",
+    };
+    const audioURL =
       "https://nt-audio-test.s3.us-east-2.amazonaws.com/people-talking-busy-restaurant.mp3";
 
     if (!sound) {
       await loadAudio(audioURL);
     }
 
-    await video.current?.loadAsync({ uri: videoURL }, {}, true);
+    await video.current?.loadAsync({ uri: videoURLs[language] }, {}, true);
     await sound?.setStatusAsync({ volume, shouldPlay: true });
 
     video.current?.playAsync();
@@ -52,10 +56,16 @@ export default function App() {
       />
       <Text style={styles.title}>Adjust Background Volume:</Text>
       <View style={styles.buttonContainer}>
-        <Button title="Low" onPress={() => playVideoAndAudio(0.1)} />
+        <Button title="Very Low" onPress={() => playVideoAndAudio(0.05)} />
+        <Button title="Low" onPress={() => playVideoAndAudio(0.25)} />
         <Button title="Medium" onPress={() => playVideoAndAudio(0.5)} />
-        <Button title="High" onPress={() => playVideoAndAudio(1.0)} />
+        <Button title="High" onPress={() => playVideoAndAudio(0.75)} />
+        <Button title="Very High" onPress={() => playVideoAndAudio(1.0)} />
       </View>
+      <Button
+        title={`Switch to ${language === "EN" ? "Spanish" : "English"}`}
+        onPress={() => setLanguage(language === "EN" ? "ES" : "EN")}
+      />
     </View>
   );
 }
